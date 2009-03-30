f82add24a4025c32a2e3fd44dc614ec489e6077f
John D. Hume <duelin.markers@gmail.com>
2009-03-30 01:33:02 -0400

    Docs.

--- src/appengine_clj/users.clj
+++ src/appengine_clj/users.clj
@@ -3,11 +3,17 @@
     (com.google.appengine.api.users User UserService UserServiceFactory)))
 
 
-(defn user-info []
+(defn user-info
+  "Returns a UserService and User for the current request in a map keyed by :user-service and :user respectively.
+  if the user is not logged in, :user will be nil."
+  []
   (let [user-service (UserServiceFactory/getUserService)]
     {:user (.getCurrentUser user-service) :user-service user-service}))
 
-(defn wrap-with-user-info [application]
+(defn wrap-with-user-info
+  "Ring middleware method that wraps an application so that every request will have
+  a user-info map assoc'd to the request under the key :appengine-clj/user-info."
+  [application]
   (fn [request]
     (application (assoc request :appengine-clj/user-info (user-info)))))
 
