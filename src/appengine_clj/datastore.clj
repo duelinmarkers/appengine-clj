a581860d657c9dcef3d2ede7902a1eba54d588e3
John D. Hume <duelin.markers@gmail.com>
2009-04-05 01:19:19 -0400

    Type-hint arguments.

--- src/appengine_clj/datastore.clj
+++ src/appengine_clj/datastore.clj
@@ -1,18 +1,23 @@
 (ns appengine-clj.datastore
-  (:import (com.google.appengine.api.datastore DatastoreServiceFactory)))
+  (:import (com.google.appengine.api.datastore
+             DatastoreServiceFactory
+             Entity
+             Query)))
 
 
 (defn entity-to-map
   "Converts an instance of com.google.appengine.api.datastore.Entity
   to a PersistentHashMap with properties stored under keyword keys,
   plus the entity's kind stored under :kind and key stored under :key."
-  [entity]
+  [#^Entity entity]
   (reduce #(assoc %1 (keyword (key %2)) (val %2))
     {:kind (.getKind entity) :key (.getKey entity)}
     (.entrySet (.getProperties entity))))
 
 (defn find-all
-  [query]
+  "Executes the given com.google.appengine.api.datastore.Query
+  and returns the results as a lazy sequence of items converted with entity-to-map."
+  [#^Query query]
   (let [data-service (DatastoreServiceFactory/getDatastoreService)
         results (.asIterable (.prepare data-service query))]
     (map entity-to-map results)))
