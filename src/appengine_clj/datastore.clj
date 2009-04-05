e82966bf47b3f43ddf1cd6ecaa81e3017dd74539
John D. Hume <duelin.markers@gmail.com>
2009-04-04 23:44:05 -0400

    Testing around find-all.

--- src/appengine_clj/datastore.clj
+++ src/appengine_clj/datastore.clj
@@ -1,12 +1,19 @@
-(ns appengine-clj.datastore)
+(ns appengine-clj.datastore
+  (:import (com.google.appengine.api.datastore DatastoreServiceFactory)))
 
 
 (defn entity-to-map
   "Converts an instance of com.google.appengine.api.datastore.Entity
   to a PersistentHashMap with properties stored under keyword keys,
-  plus the entity's kind stored under :kind."
+  plus the entity's kind stored under :kind and key stored under :key."
   [entity]
   (reduce #(assoc %1 (keyword (key %2)) (val %2))
     {:kind (.getKind entity) :key (.getKey entity)}
     (.entrySet (.getProperties entity))))
+
+(defn find-all
+  [query]
+  (let [data-service (DatastoreServiceFactory/getDatastoreService)
+        results (.asIterable (.prepare data-service query))]
+    (map entity-to-map results)))
 
--- test/appengine_clj/datastore_test.clj
+++ test/appengine_clj/datastore_test.clj
@@ -3,14 +3,29 @@
   (:use
      clojure.contrib.test-is
      appengine-clj.test-utils)
-  (:import (com.google.appengine.api.datastore Entity DatastoreServiceFactory)))
+  (:import (com.google.appengine.api.datastore
+             DatastoreServiceFactory
+             Entity
+             Query
+             Query$FilterOperator
+             Query$SortDirection)))
 
 
-(dstest converts-entity-to-persistent-map
+(dstest entity-to-map-converts-to-persistent-map
   (let [entity (doto (Entity. "MyKind")
                 (.setProperty "foo" "Foo")
                 (.setProperty "bar" "Bar"))]
     (.put (DatastoreServiceFactory/getDatastoreService) entity)
     (is (= {:foo "Foo" :bar "Bar" :kind "MyKind" :key (.getKey entity)}
            (ds/entity-to-map entity)))))
+
+(dstest find-all-runs-given-query
+  (.put (DatastoreServiceFactory/getDatastoreService)
+        [(doto (Entity. "A") (.setProperty "code" 1) (.setProperty "name" "jim"))
+         (doto (Entity. "A") (.setProperty "code" 2) (.setProperty "name" "tim"))
+         (doto (Entity. "B") (.setProperty "code" 1) (.setProperty "name" "jan"))])
+  (is (= ["jim" "tim"] (map :name (ds/find-all (doto (Query. "A") (.addSort "code"))))))
+  (is (= ["tim" "jim"] (map :name (ds/find-all (doto (Query. "A") (.addSort "code" Query$SortDirection/DESCENDING))))))
+  (is (= ["jim"] (map :name (ds/find-all (doto (Query. "A") (.addFilter "code" Query$FilterOperator/EQUAL 1))))))
+  (is (= ["jan"] (map :name (ds/find-all (doto (Query. "B") (.addFilter "code" Query$FilterOperator/EQUAL 1)))))))
 
