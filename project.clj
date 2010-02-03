b3c22b883ce144f4950b11540f3c8fbb2bf588c2
Roman Scherer <roman.scherer@burningswell.com>
2010-02-03 22:57:34 +0800

    Added leiningen project file.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject appengine-clj "0.0.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.1.0"]
+                     [com.google/appengine-api-1.0-sdk "1.3.0"]
+                     [com.google/appengine-api-labs "1.3.0"]
+                     [com.google/appengine-api-stubs "1.3.0"]
+                     [com.google/appengine-local-runtime "1.3.0"]])
